fb1dfd4127cad3a32dcb836b23e1afbb43103735
alkryukov <alkryukov@gmail.com>
2020-02-09 20:46:00 +0300

    Web calc MVP. No history load/safe. No sqrt, exp etc.

--- browser_calc/js/browser_calc.js
+++ browser_calc/js/browser_calc.js
@@ -1,25 +1,70 @@
 window.onload = function() {
-  var calcArea = document.getElementById("calc_area");
+  var calcArea = document.getElementById('calc_area');
 
   calcArea.addEventListener('keyup', (e) => {
     var key = e.keyCode || e.which;
     let enterKeyCode = 13;
     if (key == enterKeyCode) {
-      calcArea.value += calculate(calcArea.value);
+      var position = calcArea.selectionEnd;
+      var txtAboveCursor = calcArea.value.substring(0, position);
+      var txtBelowCursor = calcArea.value.substring(position);
+
+      var calcResult = calculate(txtAboveCursor);
+
+      calcArea.value = txtAboveCursor + calcResult + txtBelowCursor;
+      calcArea.selectionEnd = position + calcResult.length;
     };
   });
 };
 
 function calculate(rawText) {
-  equationText = searchBottomEquation(rawText);
-  calcResult = calculateFromEquation(equationText);
+  var equationText = searchBottomEquation(rawText);
+  var calcResult = calculateFromEquation(equationText);
   return calcResult;
-}
+};
 
 function searchBottomEquation(rawText) {
-  return rawText;
-}
+  var textLines = rawText.split('\n');
+  var equationLine = '';
+  if (textLines.length >= 2) {
+    equationLine = textLines[textLines.length - 2];
+  }
+  console.log(equationLine);
+  return equationLine;
+};
 
 function calculateFromEquation(equationText) {
-  return "__Value__";
-}
+  var validatedEquation = validateEquation(equationText);
+  var calculatedValue = '';
+  if (validatedEquation != '') {
+    calculatedValue = eval(validatedEquation).toString()
+  };
+  if (calculatedValue == validatedEquation) {
+    calculatedValue = '';
+  }
+  return calculatedValue;
+};
+
+function validateEquation(equation) {
+  const blacklistRe = /[,\\]/;
+  var stopMatch = equation.match(blacklistRe);
+  console.log(stopMatch);
+  const equationRe = /^[0-9.+*-/()]+$/;
+  var eqMatch = equation.match(equationRe);
+  console.log(eqMatch);
+
+  var validEquation = '';
+  if (stopMatch == null && eqMatch != null)
+    validEquation = equation;
+  return validEquation;
+};
+
+function restoreHistory() {
+  console.log("restoreHistory is a dummy function. To be implemented.");
+  return '';
+};
+
+function saveHistory() {
+  console.log("saveHistory is a dummy function. To be implemented.");
+  return '';
+};
