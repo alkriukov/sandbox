37d5d3b1ab36a12788d99a08267da2926a322403
alkryukov <alkryukov@gmail.com>
2020-09-01 00:09:47 +0300

    Run re-map conditionally if device present

--- reassign_mouse_buttons/remap_logitech_buttons.sh
+++ reassign_mouse_buttons/remap_logitech_buttons.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 logitech_mouse_id=$(xinput list | grep "Logitech USB Receiver  " | sed 's/^.*id=\([0-9]*\)[ \t].*$/\1/')
-echo $logitech_mouse_id > /tmp/my_mouse_id.txt
-xinput set-button-map $logitech_mouse_id 1 2 3 4 5 6 7 3 8
+if [ -z $logitech_mouse_id ]
+then
+    echo Logitech Mouse Not Found > /tmp/my_mouse_id.txt
+else
+    echo $logitech_mouse_id > /tmp/my_mouse_id.txt
+    xinput set-button-map $logitech_mouse_id 1 2 9 4 5 6 7 3 8
+fi
 
