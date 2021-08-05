11502154bb3d9172a81d895efbc3804400d19d1f
alkryukov <alkryukov@gmail.com>
2021-08-06 00:08:48 +0300

    PoC - no functions support, no buttons functioning, no graphs, no quick access

--- eng_calc/eng_calc.py
+++ eng_calc/eng_calc.py
@@ -11,7 +11,7 @@ def form_menu(menu_frame):
     file_btn.pack(side=Tk.LEFT)
     file_btn = Tk.Button(text='Save As', master = menu_frame)
     file_btn.pack(side=Tk.LEFT)
-    file_btn = Tk.Button(text='Copy Graph', master = menu_frame)
+    file_btn = Tk.Button(text='Save Graph', master = menu_frame)
     file_btn.pack(side=Tk.LEFT)
     sets_btn = Tk.Button(text='Settings', master = menu_frame)
     sets_btn.pack(side=Tk.LEFT)
@@ -37,13 +37,22 @@ def form_calc(calc_frame):
     calc_text_area.bind('<Return>', on_calc_enter)
 
 
-def on_calc_enter(a):
-    print(a)
-    print('Enter pressed')
+def on_calc_enter(event):
+    text = event.widget.get('1.0', Tk.INSERT).split('\n')[-1]
+    result = ''
+    return_value = ''
+    try:
+        result = eval(text)
+        if str(result) != str(text):
+            event.widget.insert(Tk.INSERT, '\n' + str(result))
+            return_value = 'break'
+    except (NameError, ValueError, SyntaxError) as e:
+        print(e)
+    return return_value
 
 
 home_window = Tk.Tk()
-home_window.geometry('500x700')
+home_window.geometry('600x700')
 
 calc_frames = {
     'menu': Tk.Frame(width=80),
@@ -53,12 +62,12 @@ calc_frames = {
 }
 
 form_menu(calc_frames['menu'])
-calc_frames['menu'].place(x=0, y=0, width=500, height=25)
+calc_frames['menu'].place(x=0, y=0, width=600, height=25)
 form_quick_acc(calc_frames['quick'])
-calc_frames['quick'].place(x=0, y=25, width=500, height=25)
+calc_frames['quick'].place(x=0, y=25, width=600, height=25)
 form_graph(calc_frames['graph'])
-calc_frames['graph'].place(x=0, y=50, width=500, height=300)
+calc_frames['graph'].place(x=0, y=50, width=600, height=300)
 form_calc(calc_frames['calc'])
-calc_frames['calc'].place(x=0, y=350, width=500, height=350)
+calc_frames['calc'].place(x=0, y=350, width=600, height=350)
 
 home_window.mainloop()
