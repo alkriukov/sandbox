767d86df58ace2065c921fa92e316b4980727842
alkryukov <alkryukov@gmail.com>
2022-04-12 09:40:25 +0300

    Change awaiting command to create rabbitmq user

--- learn_and_poc/flask_sqlalchemy/rabbit/init.sh
+++ learn_and_poc/flask_sqlalchemy/rabbit/init.sh
@@ -2,9 +2,9 @@
 
 # Create Rabbitmq user
 ( rabbitmqctl wait --timeout 60 $RABBITMQ_PID_FILE ; \
-rabbitmqctl add_user $RABBITMQ_USER $RABBITMQ_PASSWORD 2>/dev/null ; \
-rabbitmqctl set_user_tags $RABBITMQ_USER administrator ; \
-rabbitmqctl set_permissions -p / $RABBITMQ_USER  ".*" ".*" ".*" ; \
+rabbitmqctl add_user username userpass 2>/dev/null ; \
+rabbitmqctl set_user_tags username administrator ; \
+rabbitmqctl set_permissions -p / username  ".*" ".*" ".*" ; \
 echo "*** User '$RABBITMQ_USER' with password '$RABBITMQ_PASSWORD' completed. ***" ; \
 echo "*** Log in the WebUI at port 15672 (example: http:/localhost:15672) ***") &
 
