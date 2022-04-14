0e47f0a51b38732c69c835dd9ccd95e3bc9d5f29
alkryukov <alkryukov@gmail.com>
2022-04-14 16:12:58 +0300

    Move crm project into learn and poc

--- learn_and_poc/enterprize/enterprize/settings.py
+++ learn_and_poc/enterprize/enterprize/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,8 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&e4kwqkgx3%%q(in*(f678ygg-0dv_-a@p!o*ywypu1hswo5hz'
-
+# 'django-insecure-&e4kwqkgx3%%q(in*(f678ygg-0dv_-a@p!o*ywypu1hswo5hz'
+if not os.environ.get('DJANGO_KEY'):
+    os.environ['DJANGO_KEY'] = 'blank_value_for_debugging'
+SECRET_KEY = os.environ.get('DJANGO_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
