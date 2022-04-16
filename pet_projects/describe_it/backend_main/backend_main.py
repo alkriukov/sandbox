81971b0789301e88e6230f3ec480e6b90f0b23c9
alkryukov <alkryukov@gmail.com>
2022-04-17 01:31:14 +0300

    Added VoteConnect class and API for votes

--- pet_projects/describe_it/backend_main/backend_main.py
+++ pet_projects/describe_it/backend_main/backend_main.py
@@ -1,4 +1,3 @@
-
 import os, pika, time, json
 
 from flask import Flask, request
@@ -34,6 +33,21 @@ class TagConnect(db.Model):
     next_id = db.Column(db.Integer, nullable=False)
     UniqueConstraint('base_id', 'next_id', name='base_next_unique')
 
+@dataclass
+class VoteConnect(db.Model):
+    id: int
+    base_id: int
+    vote_id: int
+    upvotes: int
+    downvotes: int
+    id = db.Column(db.Integer, primary_key=True)
+    base_id = db.Column(db.Integer, nullable=False)
+    vote_id = db.Column(db.Integer, nullable=False)
+    upvotes = db.Column(db.Integer, nullable=False)
+    downvotes = db.Column(db.Integer, nullable=False)
+    UniqueConstraint('base_id', 'vote_id', name='base_vote_unique')
+
+
 this_is_prod = True
 flask_env = os.environ.get('FLASK_ENV')
 if flask_env and flask_env == 'development':
@@ -44,11 +58,6 @@ if flask_env and flask_env == 'development':
 def index():
     return '<p>Hello</p>'
 
-@app.route('/initDb/', methods=['GET'])
-def init_db():
-    db.create_all()
-    return '<p>Init Completed!</p>'
-
 
 @app.route('/api/tags/', methods=['GET'])
 def showTags():
@@ -57,6 +66,11 @@ def showTags():
     for t in current_tags:
         tags_to_show.append(str(t.text))
     return ' '.join(tags_to_show)
+
+@app.route('/api/votes/', method=['GET'])
+def showVotes():
+    return 'Hello'
+
 
 @app.route('/api/tag/<tagname>/', methods=['GET', 'PUT', 'POST', 'DELETE'])
 def changeTag(tagname):
@@ -82,9 +96,14 @@ def changeTag(tagname):
         resp_text = str(tagname) + ' > ' + str(new_text)
     elif request.method == 'DELETE':
         tag_to_del = Tag.query.filter_by(text=tagname).first()
-        con_to_del = TagConnect.query.filter(or_(TagConnect.base_id==tag_to_del.id, TagConnect.next_id==tag_to_del.id))
+        con_to_del = TagConnect.query.filter(
+            or_(TagConnect.base_id==tag_to_del.id, TagConnect.next_id==tag_to_del.id))
         for c in con_to_del:
             db.session.delete(c)
+        vote_to_del = VoteConnect.query.filter(
+            or_(VoteConnect.base_id==tag_to_del.id, VoteConnect.vote_id==tag_to_del.id))
+        for v in vote_to_del:
+            db.session.delete(v)
         db.session.delete(tag_to_del)
         db.session.commit()
         resp_text = str(tagname) + ' deleted'
@@ -114,7 +133,8 @@ def changeConnection():
         json_body = json.loads(request.get_data())
         base_tag = Tag.query.filter_by(text=json_body['base']).first()
         next_tag = Tag.query.filter_by(text=json_body['next']).first()
-        con_to_del = TagConnect.query.filter(and_(TagConnect.base_id==base_tag.id, TagConnect.next_id==next_tag.id))
+        con_to_del = TagConnect.query.filter(
+            and_(TagConnect.base_id==base_tag.id, TagConnect.next_id==next_tag.id))
         for c in con_to_del:
             db.session.delete(c)
         db.session.commit()
@@ -123,6 +143,51 @@ def changeConnection():
         resp_text = 'Unsupported request method'
     return resp_text
 
+def vote(base_tag_text, vote_tag_text, up_or_down):
+    base_tag = Tag.query.filter_by(text=base_tag_text).first()
+    vote_tag = Tag.query.filter_by(text=vote_tag_text).first()
+    con_to_vote = VoteConnect.query.filter(
+        and_(VoteConnect.base_id==base_tag.id, VoteConnect.vote_id==vote_tag.id)).first()
+    if con_to_vote:
+        if up_or_down == 'up':
+            con_to_vote.upvotes += 1
+        elif up_or_down == 'down':
+            con_to_vote.downvotes += 1
+    else:
+        if up_or_down == 'up':
+            db.session.add(
+                VoteConnect(base_id=base_tag.id, vote_id=vote_tag.id, upvotes=1, downvotes=0))
+        elif up_or_down == 'down':
+            db.session.add(
+                VoteConnect(base_id=base_tag.id, vote_id=vote_tag.id, upvotes=0, downvotes=1))
+    db.session.commit()
+
+@app.route('/api/vote/up', methods=['PUT', 'POST'])
+def voteUp():
+    json_body = json.loads(request.get_data())
+    vote(json_body['base'], json_body['vote'], 'up')
+    return 'Vote Up'
+
+@app.route('/api/vote/down', methods=['PUT', 'POST'])
+def voteDown():
+    json_body = json.loads(request.get_data())
+    vote(json_body['base'], json_body['vote'], 'down')
+    return 'Vote Down'
+
+@app.route('/api/vote/down', methods=['POST'])
+def setVotes():
+    json_body = json.loads(request.get_data())
+    base_tag = Tag.query.filter_by(text=json_body['base']).first()
+    vote_tag = Tag.query.filter_by(text=json_body['vote']).first()
+    up_or_down = json_body['up_or_down']
+    votes = json_body['votes']
+    con_to_vote = VoteConnect.query.filter(
+        and_(VoteConnect.base_id==base_tag.id, VoteConnect.vote_id==vote_tag.id)).first()
+    if up_or_down == 'up':
+        con_to_vote.upvotes = votes
+    elif up_or_down == 'down':
+        con_to_vote.downvotes = votes
+    db.session.commit()
 
 
 if __name__ == "__main__":
