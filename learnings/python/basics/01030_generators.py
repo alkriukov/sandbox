b4824f779ecbd0b69de7fb8ffe833d75946752fd
alkryukov <alkryukov@gmail.com>
2022-06-14 01:21:33 +0300

    +generators, +pytest

---
+++ learnings/python/basics/01030_generators.py
@@ -0,0 +1,17 @@
+def square_gen():
+    i = 0
+    max = 10
+    while i < max:
+        square = i * i
+        i += 1
+        yield square
+
+squares = square_gen()
+print(next(squares))
+print(next(squares))
+print(next(squares))
+print(next(squares))
+
+print()
+for item in square_gen():
+    print(item)
