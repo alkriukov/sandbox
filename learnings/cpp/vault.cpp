914463a78de8447cdf1c8cc2d4f132c2ed054c6c
Aleksei Kriukov <ak@ak-mac.speedport.ip>
2023-04-07 15:18:50 +0200

    password compare

--- learnings/cpp/vault.cpp
+++ learnings/cpp/vault.cpp
@@ -5,6 +5,15 @@ using namespace std;
 int main()
 {
     string password = "hello_password";
-    cout << password;
+    string flag = "flag{sfli23i23fpfo2}";
+    string attempt_p;
+    cout << password << endl;
+    cin >> attempt_p;
+    if (attempt_p == password) {
+        cout << flag;
+    }
+    else {
+        cout << "The password is not correct" << endl;
+    }
     return 0;
 }
