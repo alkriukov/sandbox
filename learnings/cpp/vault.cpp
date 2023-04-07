089708be613a50f1c67e5e8654c47ad03e53e3bc
Aleksei Kriukov <ak@ak-mac.speedport.ip>
2023-04-07 22:00:54 +0200

    Greeting

--- learnings/cpp/vault.cpp
+++ learnings/cpp/vault.cpp
@@ -5,9 +5,9 @@ using namespace std;
 int main()
 {
     string flag = "flag{never_keep_secrets_in_code}";
-    string correct_pass = "I_Feel_Lucky_Today";
+    string correct_pass = "iFeelLuckyT0d@y";
     string attempt_pass;
-    cout << "Enter password" << endl;
+    cout << "The password is 10+ symbols long with Capital and lowercase symbols,\nditits and special symbols!\nGood luck guessing it: " << endl;
     cin >> attempt_pass;
     if (attempt_pass == correct_pass) {
         cout << flag << endl;
