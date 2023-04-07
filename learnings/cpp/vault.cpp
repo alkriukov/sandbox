6e239ae99ded4beefc65157b4fda6677e2837758
Aleksei Kriukov <ak@ak-mac.speedport.ip>
2023-04-07 15:20:58 +0200

    endlines

--- learnings/cpp/vault.cpp
+++ learnings/cpp/vault.cpp
@@ -4,13 +4,13 @@ using namespace std;
 
 int main()
 {
-    string password = "hello_password";
     string flag = "flag{sfli23i23fpfo2}";
-    string attempt_p;
-    cout << password << endl;
-    cin >> attempt_p;
-    if (attempt_p == password) {
-        cout << flag;
+    string correct_pass = "hello_password";
+    string attempt_pass;
+    cout << "Enter password" << endl;
+    cin >> attempt_pass;
+    if (attempt_pass == correct_pass) {
+        cout << flag << endl;
     }
     else {
         cout << "The password is not correct" << endl;
