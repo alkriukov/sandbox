cee56485992971244f3b60e50e8094fed6a5b1ed
Aleksei Kriukov <ak@ak-mac.speedport.ip>
2023-04-10 00:20:52 +0200

    Changed flag, slightly complicated strings usage

--- learnings/cpp/vault.cpp
+++ learnings/cpp/vault.cpp
@@ -4,16 +4,16 @@ using namespace std;
 
 int main()
 {
-    string flag = "flag{never_keep_secrets_in_code}";
+    string flag = "n3v3r_k33p_s3cr3ts_1n_c0d3";
     string correct_pass = "iFeelLuckyT0d@y";
     string attempt_pass;
-    cout << "The password is 10+ symbols long with Capital and lowercase symbols,\nditits and special symbols!\nGood luck guessing it: " << endl;
+    cout << "The password is 10+ symbols long with Capital and lowercase symbols,\nditits and special symbols!\nGood luck guessing it:\n";
     cin >> attempt_pass;
     if (attempt_pass == correct_pass) {
-        cout << flag << endl;
+        cout << "flag{" << flag << "}\n";
     }
     else {
-        cout << "The password is not correct" << endl;
+        cout << "The password is not correct. No wonder why! Loser.." << endl;
     }
     return 0;
 }
